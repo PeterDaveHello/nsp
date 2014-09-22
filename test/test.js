4b293baacdcb17f51f8c85020326e637f458c8b9
tomsteele <thomasjsteele@gmail.com>
2014-09-22 06:12:24 -0700

    Cleanup fixtures and move tests to lab

--- test/test.js
+++ test/test.js
@@ -1,29 +1,37 @@
-var assert = require('assert');
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
 var sinon = require('sinon');
 var rewire = require('rewire');
 
 var auditPackage = rewire('./../lib/auditPackage');
 var fixtures = require('./data/fixtures');
+var suite = lab.suite;
+var before = lab.before;
+var after = lab.after;
+var assert = Lab.assert;
+var it = lab.it;
 
-describe('auditPackage', function () {
+suite('auditPackage', function () {
     var validateStub;
     var sandbox = sinon.sandbox.create();
 
-    before(function () {
+    before(function (done) {
         validateStub = sandbox.stub();
 
         validateStub.withArgs('qs').yields(null, fixtures['qs@0.5.x']);
         validateStub.yields(null, []);
 
         auditPackage.__set__('validateModule', validateStub);
+        done();
     });
 
-    after(function () {
+    after(function (done) {
         sandbox.restore();
+        done();
     });
 
     it('should return a list of advisory for a vulnerable package', function (done) {
-        auditPackage('tests/data/vulnerable-package.json', function (err, results) {
+        auditPackage('test/data/vulnerable-package.json', function (err, results) {
             var qs = fixtures.qsVulnerabilityResponse;
 
             assert.ifError(err);
@@ -35,7 +43,7 @@ describe('auditPackage', function () {
     });
 
     it('should return a valid list of ancestors', function (done) {
-        auditPackage('tests/data/transitive-dependency.json', function (err, results) {
+        auditPackage('test/data/transitive-dependency.json', function (err, results) {
             var ancestry = ['root@0.0.1', 'couchbase@1.2.2', 'request@2.30.0', 'qs@0.6.6'];
             assert.ifError(err);
             assert.equal(1, results.length);
@@ -48,7 +56,7 @@ describe('auditPackage', function () {
     it('should return an empty result list with git dependencies', function (done) {
         validateStub.returns([]);
 
-        auditPackage('tests/data/git-deps-package.json', function (err, results) {
+        auditPackage('test/data/git-deps-package.json', function (err, results) {
             assert.ifError(err);
             assert.equal(0, results.length);
 
